49f25dae8b3858c678bc26ebbaabbb94c5390344
abdellah ait bachikh <abdo.jhon2023@gmail.com>
2025-10-20 19:19:53 +0100

    create project & initilise ui librarys

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@heroui/react";
+
+const App = () => {
+  return (
+    <div className="h-screen">
+      <div className="underline   flex items-center justify-center">App</div>
+      <Button>test</Button>
+    </div>
+  );
+};
+
+export default App;
